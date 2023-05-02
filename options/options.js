34f486a2e98f1491b6cf0ff7634012e45fadc80e
Shamshir Khan <khanshamshir428@gmail.com>
2023-05-02 13:24:57 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "cup-cut-40391",
   url: "https://cup-cut-40391.botics.co",
   api: "https://cup-cut-40391.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
